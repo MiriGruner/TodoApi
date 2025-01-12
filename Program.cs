948de4226bfb382aad076cf2dc2eabfcb7a32e97
MiriGruner <025860374g@gmail.com>
2025-01-12 19:25:02 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,58 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<ToDoDbContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        new MySqlServerVersion(new Version(8, 0, 32))
+    ));
+// הוספת שירותי CORS
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+
+
+
+var app = builder.Build();
+
+// הפעלת CORS
+app.UseCors();
+
+// הגדרת ה-Routes
+app.MapGet("/", () =>  "dddd");
+app.MapGet("/items", async (ToDoDbContext db) => await db.Items.ToListAsync());
+app.MapPost("/items", async (ToDoDbContext db, Item item) =>
+{
+    db.Items.Add(item);
+    await db.SaveChangesAsync();
+    return Results.Created($"/items/{item.Id}", item);
+});
+app.MapPut("/items/{id}", async (ToDoDbContext db, int id, Item updatedItem) =>
+{
+    var item = await db.Items.FindAsync(id);
+    if (item == null) return Results.NotFound();
+
+    item.Name = updatedItem.Name;
+    item.IsComplete = updatedItem.IsComplete;
+
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+app.MapDelete("/items/{id}", async (ToDoDbContext db, int id) =>
+{
+    var item = await db.Items.FindAsync(id);
+    if (item == null) return Results.NotFound();
+
+    db.Items.Remove(item);
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+
+app.Run();
